605a06e9bd8d6c5fc45cc3ccacbfdc00a4af9092
HyunJungOh <hjoh@selva-i.co.jp>
2019-03-31 07:00:53 +0900

    deleted hard-coded lists from HTML, changed makeElement function for flexbox style

--- sample.js
+++ sample.js
@@ -13,73 +13,113 @@ var mailOptions = {
     new     : '新規',
     format  : '既定'
 };
+
+var Price =[
+
+{
+    unitPrice :'0',
+    qty:'1',
+//    netPrice: unitPrice * qty,
+},
+];
+
 var totalPrice = 0;
 //add first list item
 $("#btn").on("click", function(){
+    $(this).attr("disabled", true);
     makeSelect();
     });
 // add second item
-$(document).on("click", ".plus_btn",function(){
+$(document).on("click", ".plus-btn",function(){
     makeSelect();
     });
 // delete list item
-$(document).on("click", ".delete_btn",function(){
-    console.log("a");
-    $(this).parents("div .item").empty();
+$(document).on("click", ".delete-btn",function(){
+    $(this).parents("div .item").remove();
     });
 
 
 //add Radio options
 $(document).on("change", ".category",function(){
     var selectedOption = $(this).val();
-    var appendDiv = $(this).parent('div');
+    var appendDiv = $(this).parent('div').parent('div');
+//console.log(this);
     if (selectedOption !== 'none' && selectedOption === 'tel'){
-        makeElement(telOptions, appendDiv);
+        makeElement(telOptions, appendDiv, 'length');
     } else if (selectedOption !== 'none' && selectedOption === 'email'){
-        makeElement(mailOptions,appendDiv);
+        makeElement(mailOptions,appendDiv, 'mail_kind');
         } else if (selectedOption !== 'none' && selectedOption === 'deal'){
-        console.log('deal');
-    }
+        makeElement('',appendDiv, 'num');
+        }
 })
 
 function makeSelect(){
     var $item   = $('<div>').addClass('item')
-    $select = $('<select>')
-    .addClass("category")
+    var $col1 = $('<div>').addClass('col1');
+    $select = $('<select>').addClass("category")
 
-    $(".list").append($item)
-    $item.append($select);
+    $(".list").append($item);
+    $item.append($col1);
+    $col1.append($select);
     $.each(options, function(key, val){
         $option = $('<option>').val(key).text(val)
         $select.append($option)
     })
 }
-function makeElement(optionObj, eventPlace){
+
+function makeElement(optionObj, eventPlace, name){
+    var $container= $('<div>').addClass('container');
+    var $col2     = $('<div>').addClass('col2');
+    var $col3     = $('<div>').addClass('col3');
+    var $col4     = $('<div>').addClass('col4');
+    var $col5     = $('<div>').addClass('col5');
+
     var $input    = $("<input type='text'>").addClass("num").attr('placeholder','件数');
-    var $netPrice = $("<span>").addClass("net_price");
+    var $netPrice = $("<span>").addClass("net-price");
     var $buttons  = $("<span>").addClass("buttons");
-    var $addBtn   = $("<button>").addClass("plus_btn"); 
-    var $deleteBtn= $("<button>").addClass("delete_btn");
+    var $addBtn   = $("<button>").addClass("plus-btn"); 
+    var $deleteBtn= $("<button>").addClass("delete-btn");
     var $btn      = $buttons.append($addBtn).append($deleteBtn);
-    $.each(optionObj, function(key, val){
-        var $label = $('<label>').text(val);
-        $label.appendTo(eventPlace);
-        $telOption = $('<input type="radio">').val(key).attr('name', 'length')
-        $label.prepend($telOption)
-    });
+
+    if (eventPlace.find('input[type=text]').attr('class')){
+        eventPlace.find('.container').remove();
+    } 
     
-    eventPlace.append($input).append('件')
-              .append($netPrice).append('円')
-              .append($btn);
-}
+    $container.appendTo(eventPlace);
+    $col2.appendTo($container);
+    $col3.appendTo($container);
+    $col4.appendTo($container);
+    $col5.appendTo($container);
+
+    if (optionObj){
+        $.each(optionObj, function(key, val){
+        var $label = $('<label>').text(val);
+        $col2.appendTo($container);
+        $label.appendTo($col2);
+        $telOption = $('<input type="radio">').val(key).attr('name', name)
+        $label.prepend($telOption)
+        });
+    }
+    $col3.append($input).append('件');
+    $col4.append($netPrice).append('円');
+    $col5.append($btn);
+    $container.append($col3).append($col4).append($col5);
+            }
 
 /*phone call radio change 
  * 10 or less 500 yen (set)
  * 
  * 10 or more 1000 yen (set)
  */
+// $(document).on("change", "input[type=radio][name=length]", function(){
+//     console.log("moved");
+// })
+
+$("input[type=radio][name=length]").on("change", function(){
+    console.log("moved");
+})
 
 /* phone call times
  * set * times 
  *  show on net & total
- */ 
+//  */ 
